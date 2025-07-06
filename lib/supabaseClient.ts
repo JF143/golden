0f2edba7388ee0024dd9c6325cc92b2e16841c13
StudHue Developer <user@example.com>
2025-07-06 21:02:17 +0800

    Fix Supabase env variable usage

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const NEXT_PUBLIC_SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY); 
+export const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+console.log('Supabase URL:', NEXT_PUBLIC_SUPABASE_URL);
+console.log('Supabase Key:', NEXT_PUBLIC_SUPABASE_ANON_KEY);
